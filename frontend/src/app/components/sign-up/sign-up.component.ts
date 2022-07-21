61b9c91d3d638b47b3cc592e6b80cae9cc86a846
Lam Jia Seng <jia-seng.lam@bassnet.no>
2022-07-21 21:43:09 +0800

    remove error message when navigate away from sign up component

--- frontend/src/app/components/sign-up/sign-up.component.ts
+++ frontend/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Select, Store } from '@ngxs/store';
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { AuthDTO } from 'src/app/models/auth-dto';
 import { Signup } from 'src/app/store/auth/auth-action';
+import { ResetError } from 'src/app/store/core/app-action';
 import { AppState } from 'src/app/store/core/app-state';
 
 @Component({
@@ -13,13 +14,17 @@ import { AppState } from 'src/app/store/core/app-state';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
   @Select(AppState.getError)
   error$!: Observable<any>;
 
   constructor(private store: Store, private router: Router) {}
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.store.dispatch(new ResetError());
+  }
 
   onSubmit(signupForm: NgForm) {
     if (signupForm.valid) {
